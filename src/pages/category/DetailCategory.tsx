6a345ed0f273f02532069c330608dc58d3870337
ryokf <ryokhrisnaf@gmail.com>
2024-06-07 21:11:29 +0700

    ✨feature: detail category responsive

--- src/pages/category/DetailCategory.tsx
+++ src/pages/category/DetailCategory.tsx
@@ -17,26 +17,28 @@ const DetailCategory = () => {
 
     return (
         <HomeTemplate>
-            <div className="">
-                <div className="w-full flex justify-center">
-                    <div className="w-40 my-10 text-primary">
-                        {categoryData[0].icon}
+            <div className="lg:mx-10">
+                <div className="grid grid-cols-1 md:grid-cols-3 md:w-3/4 lg:mx-20">
+                    <div className="w-full flex justify-center">
+                        <div className="w-40 my-10 text-primary">
+                            {categoryData[0].icon}
+                        </div>
+                    </div>
+                    <div className="mx-6 md:flex md:col-span-2 flex-col justify-center items-start">
+                        <h1 className="text-2xl font-bold">{categoryData[0].name}</h1>
+                        <p className='text-gray'>{categoryData[0].desc}</p>
                     </div>
                 </div>
-                <div className="mx-6">
-                    <h1 className="text-2xl font-bold">{categoryData[0].name}</h1>
-                    <p className='text-gray'>{categoryData[0].desc}</p>
-                </div>
-                <div className="w-11/12 m-auto">
-                {
-                    [1, 2, 3, 4, 5, 6].map((item) => {
-                        return (
-                            <div key={item} className="my-10 flex justify-center">
-                                <Card></Card>
-                            </div>
-                        )
-                    })
-                }
+                <div className="w-11/12 md:my-10 m-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-0 sm:gap-4">
+                    {
+                        [1, 2, 3, 4, 5, 6].map((item) => {
+                            return (
+                                <div key={item} className="my-6 md:my-2 flex justify-center">
+                                    <Card></Card>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
             </div>
         </HomeTemplate>
