4398fe356d7b450090c14e60c7775e92bd4d3515
ryokf <ryokhrisnaf@gmail.com>
2024-06-10 07:52:00 +0700

    🖥️ wip: project detail

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import RegisterPage from './pages/auth/RegisterPage.tsx';
 import RegisterRole from './pages/auth/RegisterRole.tsx';
 import DetailCategory from './pages/category/DetailCategory.tsx';
 import SearchResult from './pages/search/SearchResult.tsx';
+import ProjectDetail from './pages/project/ProjectDetail.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -19,6 +20,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Route path="/register/:role" element={<RegisterPage />} />
         <Route path="/category/:id" element={<DetailCategory />} />
         <Route path="/search" element={<SearchResult/>} />
+        <Route path="/project/:id" element={<ProjectDetail/>} />
         <Route path="*" element={<h1>not found</h1>} />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/project/ProjectDetail.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import HomeTemplate from '../../templates/HomeTemplate';
+import { Badge } from 'flowbite-react';
+
+const ProjectDetail = () => {
+    return (
+        <div>
+            <HomeTemplate>
+                <div className="my-10 w-11/12 m-auto ">
+
+                <div className="rounded-xl overflow-hidden">
+                    <img src="https://flowbite.com/docs/images/blog/image-1.jpg" alt="" />
+                </div>
+                <div className="my-6 mx-2">
+                    <Badge color={'blue'} className='w-20 justify-center'>Physics</Badge>
+                    <h1 className='text-xl font-medium my-2'>Lorem ipsum dolor sit amet consectetur adipisicing</h1>
+                    <div className="my-2">
+                        <p className='text-gray text-sm'>dari Rp3.000.000</p>
+                        <p className='font-medium'>terkumpul <span className='text-primary'>Rp1.500.000</span></p>
+                        <div className="my-2 w-full h-2 bg-[#ddd] rounded-full overflow-hidden">
+                            <div className="h-2 w-[25%] bg-success"></div>
+                        </div>
+                    </div>
+                    <div className="my-4">
+                        <h1>Deskripsi</h1>
+                        <p className='text-gray text-sm'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque dolore consequatur est iure, eos illo quisquam perferendis minus. Atque asperiores error, earum laboriosam dolor illo rerum eaque soluta optio perspiciatis?</p>
+                    </div>
+                </div>
+                </div>
+            </HomeTemplate>
+        </div>
+    )
+}
+
+export default ProjectDetail
