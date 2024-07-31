d3fcfb0a197feeb445af4b42eeab361be7bd104a
ryokf <ryokhrisnaf@gmail.com>
2024-07-31 08:14:11 +0700

    🐞 fix: bug in bottomNav

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -3,26 +3,22 @@ import dashboardNav from '../data/dashboardNav';
 
 const BottomNav = () => {
 
-    const navlink = dashboardNav() || []
+    const navlink = dashboardNav() as { name: string; href: string; icon: React.ReactElement; }[] || [];
 
     console.log(window.location.pathname)
 
     return (
         <div className='fixed border-2 bottom-0 bg-white w-full rounded-xl py-2'>
             <div className={`grid grid-cols-${navlink.length} my-1`}>
-                {
-                    navlink.map((item) => {
-                        return (
-                            <Link to={item.href} key={item.name} className={`${item.href == window.location.pathname ? 'text-black' : 'text-gray'} flex gap-0.5 flex-col justify-center items-center`}>
-                                {item.icon}
-                                <h1 className='text-xs font-semibold'>{item.name}</h1>
-                            </Link>
-                        )
-                    })
-                }
+                {Array.isArray(navlink) && navlink.map((item) => (
+                    <Link to={item.href} key={item.name} className={`${item.href === window.location.pathname ? 'text-black' : 'text-gray'} flex gap-0.5 flex-col justify-center items-center`}>
+                        {item.icon}
+                        <h1 className='text-xs font-semibold'>{item.name}</h1>
+                    </Link>
+                ))}
             </div>
         </div>
     )
 }
 
-export default BottomNav
+export default BottomNav;
