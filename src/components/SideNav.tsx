5692bd571e661f11207b49a726bd5e3cad2091cb
ryokf <ryokhrisnaf@gmail.com>
2024-07-31 08:19:26 +0700

    🐞 fix: bug sideNav

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -1,31 +1,36 @@
 import dashboardNav from '../data/dashboardNav'
 import { Link } from 'react-router-dom'
+import React from 'react';
+
+type NavItem = {
+    name: string;
+    href: string;
+    icon: React.ReactElement;
+};
 
 const SideNav = () => {
 
-    const navlink = dashboardNav()
+    const navlink = dashboardNav() as NavItem[];
 
-    console.log(navlink)
+    console.log(navlink);
+    console.log(window.location.pathname);
 
-    console.log(window.location.pathname)
     return (
         <div className='bg-white rounded-xl'>
-            <div className={`grid grid-row-${navlink.length} my-1 py-4`}>
+            <div className={`grid grid-rows-${navlink.length} my-1 py-4`}>
                 {
-                    navlink.map((item) => {
-                        return (
-                            <Link to={item.href} key={item.name} className={`${item.href == window.location.pathname ? 'text-black' : 'text-gray'} flex gap-2 items-center justify-start w-3/4 m-auto mb-4`}>
-                                <span>
-                                    {item.icon}
-                                </span>
-                                <h1 className='text-lg'>{item.name}</h1>
-                            </Link>
-                        )
-                    })
+                    navlink.map((item) => (
+                        <Link to={item.href} key={item.name} className={`${item.href === window.location.pathname ? 'text-black' : 'text-gray'} flex gap-2 items-center justify-start w-3/4 m-auto mb-4`}>
+                            <span>
+                                {item.icon}
+                            </span>
+                            <h1 className='text-lg'>{item.name}</h1>
+                        </Link>
+                    ))
                 }
             </div>
         </div>
     )
 }
 
-export default SideNav
+export default SideNav;
